bebef1d33cee486bfe7c9dc60cb8e26b093d4b17
Jairo-Senna <142929627+Jairo-Senna@users.noreply.github.com>
2025-10-08 13:59:32 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
-
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    // ...
+    const env = loadEnv(mode, '.', '');
     return {
-      // PROPRIEDADE CRUCIAL: Mude a base para o nome do novo repositório
-      base: '/Intima-oes-Notis-Pro/', 
-      // ...
+      // CORREÇÃO: Define o caminho base como o nome deste repositório
+      base: '/Controle-de-entregas-Log-Servi-os/', 
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+      },
+      plugins: [react()],
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
     };
 });
